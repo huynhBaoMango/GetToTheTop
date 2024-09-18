acb3a0cf59bfa514a73ee744c81bd59c187eba5b
hnghii <140951893+hnghii@users.noreply.github.com>
2024-09-18 19:54:27 +0700

    done player weapon,pickup weapon

---
+++ Assets/Script/Player/PlayerPickup.cs
@@ -0,0 +1,45 @@
+using FishNet.Object;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPickup : NetworkBehaviour
+{
+    [SerializeField] private float pickupRanger = 4f;
+    [SerializeField] private KeyCode pickupkey = KeyCode.E;
+    [SerializeField] private LayerMask pickupLayers;
+    private Transform _cameraTranform;
+
+    private PlayerWeapone _playerWeapone;
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+        if (!IsOwner)
+        {
+            enabled = false;
+            return;
+        }
+        _cameraTranform = Camera.main.transform;
+        if(TryGetComponent(out PlayerWeapone pWeapon))
+            _playerWeapone = pWeapon;
+        else
+            Debug.LogError("khong tim thay playerwearpon",gameObject);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyUp(pickupkey))
+        {
+            Pickup();
+        }
+    }
+    private void Pickup()
+    {
+        if (!Physics.Raycast(_cameraTranform.position, _cameraTranform.forward, out RaycastHit hit, pickupRanger, pickupLayers))
+            return;
+        if(hit.transform.TryGetComponent(out GroundWeapon groundWeapon))
+        {
+            _playerWeapone.InitializeWeapon(groundWeapon.PickupWeapon());
+        }
+    }
+}
---
+++ Assets/Script/Weapon/PlayerSnip.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSnip : APlayerWeapon
+{
+    public override void Fire()
+    {
+        Debug.Log(" PlayerSnip fire");
+    }
+}
