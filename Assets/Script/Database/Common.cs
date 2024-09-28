119a9416f4600a0b8541ddac9077e6502fc4807f
huynhBaoMango <140953773+huynhBaoMango@users.noreply.github.com>
2024-09-28 14:35:45 +0700

    them common script

---
+++ Assets/Script/Database/Common.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Common
+{
+
+    private static Common instance;
+    private void Awake() => instance = this;
+
+    //ham mua skin
+    //truyen vao ma skin muon mua, so tien hien co, so tien cua skin
+
+
+
+    //ham load skin
+    //load danh sach skin tu file hoac truc tiep tu database
+}
