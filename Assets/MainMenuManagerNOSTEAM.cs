e7a32032074ee5caf16b1ab78dc5ddcc20c7280f
huynhBaoMango <140953773+huynhBaoMango@users.noreply.github.com>
2024-09-30 22:59:42 +0700

    update menu

---
+++ Assets/MainMenuManagerNOSTEAM.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using FishNet;
+using FishNet.Managing;
+using FishNet.Connection;
+using FishNet.Transporting;
+
+public class MainMenuManagerNOSTEAM : MonoBehaviour
+{
+    private NetworkManager _networkManager;
+    [SerializeField] private TMP_InputField lobbyInput;
+    [SerializeField] private TextMeshProUGUI lobbyTitle, lobbyIDText;
+    [SerializeField] private GameObject pnLobby, pnJoin, btStart;
+
+    private void Start()
+    {
+        _networkManager = FindObjectOfType<NetworkManager>();
+        if (_networkManager == null)
+        {
+            Debug.LogError("NetworkManager not found, HUD will not function.");
+            return;
+        }
+
+        _networkManager.ClientManager.OnClientConnectionState += OnLobbyEntered;
+    }
+
+    private void OnLobbyEntered(ClientConnectionStateArgs obj)
+    {
+        btStart.SetActive(IsHost);
+    }
+
+    private bool IsHost => _networkManager.IsHost;
+}
