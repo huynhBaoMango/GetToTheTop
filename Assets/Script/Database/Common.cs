3e7c9ab648cb779659f1da8a99e8f83d4e814f15
ntdung261004 <ntdung261004@gmail.com>
2024-10-06 14:48:06 +0700

    ...

--- Assets/Script/Database/Common.cs
+++ Assets/Script/Database/Common.cs
@@ -10,10 +10,10 @@ using UnityEngine.Networking;
 public class Common : MonoBehaviour
 {
 
-    private static Common instance;
-    private void Awake() => instance = this;
+    public static Common instance;
+    public void Awake() => instance = this;
 
-    private List<Skin> skins = new List<Skin>();
+    //private List<Skin> skins = new List<Skin>();
 
     //Ham mua skin
     public bool PurchaseSkin(User user, Skin skin)
@@ -36,7 +36,9 @@ public class Common : MonoBehaviour
         user.ownedSkins.Add(skin.skinId);  //Them skin da mua vao nguoi dung
         skin.isPurchased = true;
 
-        //Cap nhat du lieu nguoi dung vao Database sau khi mua skin
+        // Cập nhật dữ liệu người dùng vào Database sau khi mua skin
+        UpdateUserInDatabase(user);
+
         Debug.Log($"Skin {skin.skinName} purchased successfully!");
         return true;
     }
@@ -44,7 +46,7 @@ public class Common : MonoBehaviour
 
 
     //Ham chon skin
-    public static bool SelectSkin(User user, Skin skin)
+    public bool SelectSkin(User user, Skin skin)
     {
         if (!user.ownedSkins.Contains(skin.skinId))
         {
@@ -54,58 +56,45 @@ public class Common : MonoBehaviour
 
         //Gan skin da chon cho user
         user.selectedSkin = skin.skinId;
+        // Cập nhật dữ liệu người dùng
+        UpdateUserInDatabase(user);
+
+
         Debug.Log($"{skin.skinName} selected successfully");
         return true;
     }
 
 
-    //Ham load danh sach skin tu file hoac truc tiep tu database
 
 
 
 
     // Hàm cập nhật thông tin người dùng trong Firebase
-    //private void UpdateUserInDatabase(User user)
-    //{
-    //    string databaseUrl = "https://projectm-91ec6-default-rtdb.firebaseio.com/User/" + userId + ".json";
-    //    string json = JsonUtility.ToJson(user);
+    private void UpdateUserInDatabase(User user)
+    {
+        string databaseUrl = "https://projectm-91ec6-default-rtdb.firebaseio.com/User/" + user.email + ".json";
+        string json = JsonUtility.ToJson(user);
 
-    //    StartCoroutine(UpdateUserCoroutine(databaseUrl, json));
-    //}
+        StartCoroutine(UpdateUserCoroutine(databaseUrl, json));
+    }
 
-    //private IEnumerator UpdateUserCoroutine(string url, string json)
-    //{
-    //    UnityWebRequest request = new UnityWebRequest(url, "PUT");
-    //    byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(json);
-    //    request.uploadHandler = new UploadHandlerRaw(bodyRaw);
-    //    request.downloadHandler = new DownloadHandlerBuffer();
-    //    request.SetRequestHeader("Content-Type", "application/json");
+    private IEnumerator UpdateUserCoroutine(string url, string json)
+    {
+        UnityWebRequest request = new UnityWebRequest(url, "PUT");
+        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(json);
+        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
 
-    //    yield return request.SendWebRequest();
+        yield return request.SendWebRequest();
 
-    //    if (request.result == UnityWebRequest.Result.Success)
-    //    {
-    //        Debug.Log("User updated in database successfully.");
-    //    }
-    //    else
-    //    {
-    //        Debug.LogError("Failed to update user in database: " + request.error);
-    //    }
-    //}
-
-
-
-    // Hàm lấy danh sách skin đã mua
-    //public List<Skin> GetPurchasedSkins(User user)
-    //{
-    //    List<Skin> purchasedSkins = new List<Skin>();
-    //    foreach (Skin skin in skins)
-    //    {
-    //        if (skin.isPurchased)
-    //        {
-    //            purchasedSkins.Add(skin);
-    //        }
-    //    }
-    //    return purchasedSkins;
-    //}
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            Debug.Log("User updated in database successfully.");
+        }
+        else
+        {
+            Debug.LogError("Failed to update user in database: " + request.error);
+        }
+    }
 }
