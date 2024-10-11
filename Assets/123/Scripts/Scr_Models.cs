7adce8dfb049b37335cac4ff323c7b3dedcfaf68
Freakerq <150575354+Freakerq@users.noreply.github.com>
2024-10-11 21:55:51 +0700

    Up_Player

---
+++ Assets/123/Scripts/Scr_Models.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Scr_Models : MonoBehaviour
+{
+    #region - Player -
+
+    public enum PlayerStance
+    {
+        Stand,
+        Crouch,
+        Prone,
+    }
+
+    [Serializable]
+    public class PlayerSettingsModel
+    {
+        [Header("View Settings")]
+        public float ViewXSensitivity;
+        public float ViewYSensitivity;
+
+        
+        public bool ViewXInverted;
+        public bool ViewYInverted;
+
+        [Header("Movement Settings")]
+        public bool sprintingHold;
+        public float MovementSmoothing;
+
+
+        [Header("Movement - Running")]
+        public float RunningForwardSpeed;
+        public float RunningStrafeSpeed;
+
+        [Header("Movement - Walking")]
+        public float WalkingForwardSpeed;
+        public float WalkingBackwardSpeed;
+        public float WalkingStrafeSpeed;
+
+        [Header("Jumping")]
+        public float JumpingHeight;
+        public float JumpingFalloff;
+        public float FallingSmoothing;
+
+        [Header("Speed Effectors")]
+        public float SpeedEffector = 1;
+        public float CrouchSpeedEffector;
+        public float ProneSpeedEffector;
+        public float FallingSpeedEffector;
+    }
+
+    [Serializable]
+    public class CharacterStance
+    {
+        public float CameraHeight;
+        public CapsuleCollider StanceCollider;
+    }
+
+    #endregion
+}
