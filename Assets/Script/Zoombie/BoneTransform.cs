474e424de3e13a61d10237a97176672ebf6f1aed
hnghii <140951893+hnghii@users.noreply.github.com>
2024-10-14 09:15:20 +0700

    tách code zoombie

---
+++ Assets/Script/Zoombie/BoneTransform.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoneTransform
+{
+    public Vector3 Position { get; set; }
+    public Quaternion Rotation { get; set; }
+}
+
